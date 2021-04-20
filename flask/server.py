6975e1638b4c63191cd1f654da79d065050b1c54
eureyuri <eureynoguchi@gmail.com>
2021-04-19 23:23:55 -0400

    feat: modify address and port

--- flask/server.py
+++ flask/server.py
@@ -240,4 +240,4 @@ def set_pillow_height():
 
 if __name__ == "__main__":
     connect_to_mongo()
-    app.run(debug=True, host="127.0.0.1", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=80)
