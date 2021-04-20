6ed20a087d2359769c74bd746692fc34dace0762
Da Shi <sdstar4185@gmail.com>
2021-04-21 04:19:44 +0800

    add recording functionality

--- SmartPillowApp/app/src/main/java/com/example/smartpillowapp/SecondFragment.java
+++ SmartPillowApp/app/src/main/java/com/example/smartpillowapp/SecondFragment.java
@@ -1,11 +1,24 @@
 package com.example.smartpillowapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -13,7 +26,101 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
 public class SecondFragment extends Fragment {
+
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    private LineGraphSeries<DataPoint> series;
+    private Button recordButton = null;
+    boolean mStartRecording = true;
+    private MediaRecorder recorder = null;
+    private Thread thread;
+
+    String fileName = "HELLO";
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+//        if (!permissionToRecordAccepted ) finish();
+    }
+
+    private void onRecord(boolean start) {
+        Log.i("onRecord","onRecord called!");
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile("/dev/null");
+
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+        recorder.start();
+    }
+//    private void startListenAudio() {
+//        thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while (isThreadRun) {
+//                    try {
+//                        if(bListener) {
+//                            volume = mRecorder.getMaxAmplitude();  //Get the sound pressure value
+//                            if(volume > 0 && volume < 1000000) {
+//                                World.setDbCount(20 * (float)(Math.log10(volume)));  //Change the sound pressure value to the decibel value
+//                                // Update with thread
+//                                Message message = new Message();
+//                                message.what = 1;
+//                                handler.sendMessage(message);
+//                            }
+//                        }
+//                        if(refreshed){
+//                            Thread.sleep(1200);
+//                            refreshed=false;
+//                        }else{
+//                            Thread.sleep(200);
+//                        }
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                        bListener = false;
+//                    }
+//                }
+//            }
+//        });
+
+    private void stopRecording() {
+        Log.i("stopRecording","stopRecording called!");
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+    }
+
 
     @Override
     public View onCreateView(
@@ -21,21 +128,48 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         View view = inflater.inflate(R.layout.fragment_second, container, false);
+        ActivityCompat.requestPermissions(getActivity(), permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
-        GraphView graph = (GraphView) view.findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+        series = new LineGraphSeries<DataPoint>(new DataPoint[] {
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
                 new DataPoint(3, 2),
                 new DataPoint(4, 6)
         });
+        recordButton = view.findViewById(R.id.start);
+
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
         graph.addSeries(series);
+//        graph.getViewport().setXAxisBoundsManual(true);
+//        graph.getViewport().setMinX(0);
+//        graph.getViewport().setMaxX(maxPoints);
 
         return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onRecord(mStartRecording);
+                if (mStartRecording) {
+                    recordButton.setText("Stop recording");
+                } else {
+                    recordButton.setText("Start recording");
+                }
+                mStartRecording = !mStartRecording;
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (recorder != null) {
+            recorder.release();
+            recorder = null;
+        }
     }
 }
